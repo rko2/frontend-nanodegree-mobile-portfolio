e733ff967778a69679d9c81a65446c4400130f9a
rko2 <rko2@users.noreply.github.com>
2016-03-11 13:55:59 -0600

    add responsive images plugin

--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,17 @@
 module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-responsive-images');
 
+  grunt.initConfig({
+    responsive_images: {
+      options: {
+        engine: im;
+      },
+      files: [{
+        expand: true,
+        src: ['*.{gif,jpg,png}'],
+        cwd: 'src/img',
+        dest: 'dist/img'
+      }]
+    }
+  })
 }
