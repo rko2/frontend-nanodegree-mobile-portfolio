2962e3c75978177b9b56b9ecaa0fb68f562c1949
rko2 <rko2@users.noreply.github.com>
2016-03-11 18:51:58 -0600

    remove responsive-images and mk-dir
    didn't see any use for them, was more adding them to make sure I could
    use grunt correctly

--- gruntfile.js
+++ gruntfile.js
@@ -1,35 +1,9 @@
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-responsive-images');
-  grunt.loadNpmTasks('grunt-mkdir');
 
   grunt.initConfig({
-
-    responsive_images: {
-      all: {
-        options: {
-          engine: 'im',
-          },
-          files: [{
-            expand: true,
-            src: ['*.{gif,jpg,png}'],
-            cwd: 'src/img',
-            dest: 'dist/img'
-          }]
-        }
-      },
-
-    mkdir: {
-      all: {
-        options: {
-          create: ['dist/img']
-        },
-      },
-    },
   });
 
   grunt.registerTask('default', [
-		'responsive_images',
-    'mkdir'
 	]);
 
 }
