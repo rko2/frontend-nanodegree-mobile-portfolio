6479da8d2d490fece2d00ff95a84a74cffbf5f20
rko2 <rko2@users.noreply.github.com>
2016-03-11 23:57:09 -0600

    inline css into dist index.html using grunt

--- gruntfile.js
+++ gruntfile.js
@@ -1,11 +1,22 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-responsive-images');
   grunt.loadNpmTasks('grunt-mkdir');
+  grunt.loadNpmTasks('grunt-inline-css');
 
   grunt.initConfig({
+    inlinecss: {
+        main: {
+            options: {
+            },
+            files: {
+                'dist/index.html': 'src/index.html'
+            }
+        }
+    },
   });
 
   grunt.registerTask('default', [
+    'inlinecss'
 	]);
 
 }
